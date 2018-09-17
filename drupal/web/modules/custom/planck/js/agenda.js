922f2e6267291c698facd8ceee6e987ce5601487
Dominique FABRE <dfabre@clever-age.com>
2018-09-17 04:48:53 +0200

    Fix

--- drupal/web/modules/custom/planck/js/agenda.js
+++ drupal/web/modules/custom/planck/js/agenda.js
@@ -8,24 +8,26 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 }
 var shadowAgenda = document.querySelector('[data-agenda="wrapper"]').cloneNode(true);
 var selector = document.querySelector('[data-agenda=band-selector]');
-selector.addEventListener('change', function(e) {
-    var agenda = document.querySelector('[data-agenda="wrapper"]');
-    var parent = document.querySelector('.views-element-container');
-    parent.removeChild(agenda);
-    var newAgenda = shadowAgenda.cloneNode(true);
-    if ('all' !== e.target.value) {
-        newAgenda.querySelectorAll('.plkTeaserEvent').forEach(function (event) {
-            if (e.target.value !== event.getAttribute('data-group')) {
-                event.remove();
-            }
-        });
-        newAgenda.querySelectorAll('.plkAgendaYear').forEach(function (year) {
-            if (null === year.querySelector('.plkTeaserEvent')) {
-                year.remove();
-            }
-        });        
-    }
-    parent.appendChild(newAgenda);
-    new ML.Tabs("[data-agenda=wrapper]");
-});
+if (null !== selector) {
+    selector.addEventListener('change', function (e) {
+        var agenda = document.querySelector('[data-agenda="wrapper"]');
+        var parent = document.querySelector('.views-element-container');
+        parent.removeChild(agenda);
+        var newAgenda = shadowAgenda.cloneNode(true);
+        if ('all' !== e.target.value) {
+            newAgenda.querySelectorAll('.plkTeaserEvent').forEach(function (event) {
+                if (e.target.value !== event.getAttribute('data-group')) {
+                    event.remove();
+                }
+            });
+            newAgenda.querySelectorAll('.plkAgendaYear').forEach(function (year) {
+                if (null === year.querySelector('.plkTeaserEvent')) {
+                    year.remove();
+                }
+            });
+        }
+        parent.appendChild(newAgenda);
+        new ML.Tabs("[data-agenda=wrapper]");
+    });
+}
 new ML.Tabs("[data-agenda=wrapper]");
